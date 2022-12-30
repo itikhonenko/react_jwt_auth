9b2b94d74c856302b2ccbfdf2b58cbf4dab35f55
IT086469 <IT086469@cerner.net>
2022-12-30 15:45:49 +0200

    [Feature] Add redux

--- src/index.js
+++ src/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import Welcome from './components/Welcome';
 import Signin from './components/Signin';
 import Heart from './components/Heart';
+import reducers from './reducers';
+import { createStore } from 'redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App>
-        <Routes>
-          <Route path ="/" element={<Welcome />} />
-          <Route path ="signin" element={<Signin />} />
-          <Route path ="heart" element={<Heart />} />
-        </Routes>
-      </App>
-    </BrowserRouter>
+    <Provider store={createStore(reducers, {})}>
+      <BrowserRouter>
+        <App>
+          <Routes>
+            <Route path ="/" element={<Welcome />} />
+            <Route path ="signin" element={<Signin />} />
+            <Route path ="heart" element={<Heart />} />
+          </Routes>
+        </App>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
